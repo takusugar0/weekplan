e4e0856c24a08941f0111604082b27d50849b7d7
takusugar0 <takusugar9@gmail.com>
2020-03-27 20:25:46 +0900

    taskpage

--- weekplan_app/admin.py
+++ weekplan_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Role
+from .models import Role, Task
 
 admin.site.register(Role)
+admin.site.register(Task)
--- weekplan_app/models.py
+++ weekplan_app/models.py
@@ -14,3 +14,18 @@ class Role(models.Model):
 
     def __str__(self):
         return self.title
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    status = models.IntegerField(default=1)
+    due_date =  models.DateField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    role_id = models.ForeignKey(Role, on_delete = models.CASCADE)
+
+    def publish(self):
+        self.update_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
